08a1a0a3ca4578804d5b895d382045a668934006
kpshenyshniuk <k.pshenyshniuk@abcmobile.com>
2025-01-15 11:31:32 +0100

    заменить в классе Link значение переменных на f-string

--- tests/links.py
+++ tests/links.py
@@ -1,9 +1,9 @@
 
 class Links:
     base_url = 'https://stellarburgers.nomoreparties.site/' # Линка на  страницу главную
-    link_login_page = base_url + 'login' # Линка на  страницу с авторизацией пользователя
-    link_profile_page = base_url + 'account' # Линка на траницу с Личным кабинетом пользователя
-    full_link_profile_page = base_url + 'account/profile' # линка на страницу с личным кабинетом полная линка
-    link_forgot_password_page = base_url + 'forgot-password' # Линка на страницу с восстановление пароля
-    link_registration_page = base_url + 'register' # линка на страницу с регистрацией
+    link_login_page = f'{base_url}login' # Линка на  страницу с авторизацией пользователя
+    link_profile_page = f'{base_url}account' # Линка на траницу с Личным кабинетом пользователя
+    full_link_profile_page = f'{base_url}account/profile' # линка на страницу с личным кабинетом полная линка
+    link_forgot_password_page = f'{base_url}forgot-password' # Линка на страницу с восстановление пароля
+    link_registration_page = f'{base_url}register' # линка на страницу с регистрацией
 
